eed533b441d1115317f83f5d6af2d978df135861
Karan <karanjamwal2004@gmail.com>
2025-09-16 11:55:44 +0530

    formed header

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="/chef-claude-icon.png" alt="chef-claude" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
