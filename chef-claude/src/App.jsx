f6d979c71bdb96bd1dde1a6bf163db2b6044ae95
Karan <karanjamwal2004@gmail.com>
2025-09-16 22:13:46 +0530

    use of action button

--- chef-claude/src/App.jsx
+++ chef-claude/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Form from "./components/Main";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Form />
     </>
   );
 }
---
+++ chef-claude/src/components/Main.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Main = () => {
+  const [ingredients, setIngredients] = useState([]);
+  // const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const addIngredient = (formData) => {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  };
+
+  return (
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button> + Add Ingredient</button>
+      </form>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default Main;
