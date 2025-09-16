cf2753c7936fbcf510477481452feffdda329a5a
Karan <karanjamwal2004@gmail.com>
2025-09-16 23:00:29 +0530

    radio, checkbox, select options

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 
 function App() {
-  const signUp = (formData) => {
-    const email = formData.get("email");
-    const password = formData.get("password");
-    console.log(email);
-    console.log(password);
-  };
+  
+  function signUp(formData) {
+    const data = Object.fromEntries(formData);
+    const dietaryRestrictions = formData.getAll("dietaryRestrictions");
+    const allData = {
+      ...data,
+      dietaryRestrictions,
+    };
+    console.log(allData);
+  }
 
   return (
     <section>
@@ -15,16 +19,82 @@ function App() {
         <label htmlFor="email">Email:</label>
         <input
           id="email"
+          defaultValue="joe@schmoe.com"
           type="email"
           name="email"
           placeholder="joe@schmoe.com"
         />
-        <br />
-        <label htmlFor="password">Password:</label>
-        <input id="password" type="password" name="password" />
 
-        <label htmlFor="description">Description</label>
-        <textarea name="description" id="description"></textarea>
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          defaultValue="password123"
+          type="password"
+          name="password"
+        />
+
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          name="description"
+          defaultValue="This is a description"
+        ></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="employmentStatus"
+              defaultChecked={true}
+              value="full-time"
+            />
+            Full-time
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Dietary restrictions:</legend>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+            Kosher
+          </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="vegan" />
+            Vegan
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="dietaryRestrictions"
+              defaultChecked={true}
+              value="gluten-free"
+            />
+            Gluten-free
+          </label>
+        </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select id="favColor" name="favColor" defaultValue="" required>
+          <option value="" disabled>
+            -- Choose a color --
+          </option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="violet">Violet</option>
+        </select>
 
         <button>Submit</button>
       </form>
