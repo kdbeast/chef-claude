5c4be11bdcf8fbc6bce11ab7638671c111d20714
Karan <karanjamwal2004@gmail.com>
2025-09-17 22:21:48 +0530

    using useState

--- complex-state/src/Card.jsx
+++ complex-state/src/Card.jsx
@@ -1,3 +1,4 @@
+import Star from "./components/Star";
 import avatar from "/user.png";
 
 const Card = ({ contact, toggleFavorite, starIcon }) => {
@@ -9,20 +10,11 @@ const Card = ({ contact, toggleFavorite, starIcon }) => {
         alt="User profile picture of John Doe"
       />
       <div className="info">
-        <button
-          onClick={toggleFavorite}
-          aria-pressed={contact.isFavorite}
-          aria-label={
-            contact.isFavorite ? "Remove from favorites" : "Add to favorites"
-          }
-          className="favorite-button"
-        >
-          <img
-            src={starIcon}
-            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-            className="favorite"
-          />
-        </button>
+        <Star
+          contact={contact}
+          handleClick={toggleFavorite}
+          starIcon={starIcon}
+        />
         <h2 className="name">
           {contact.firstName} {contact.lastName}
         </h2>
