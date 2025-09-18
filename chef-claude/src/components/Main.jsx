a0735493bb2f75511d20b0c8f7a87f674aaa3df7
Karan <karanjamwal2004@gmail.com>
2025-09-18 14:36:04 +0530

    deleting default ingredients

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -4,12 +4,7 @@ import IngredientsList from "./IngredientsList";
 import { getRecipeFromMistral } from "../ai";
 
 const Main = () => {
-  const [ingredients, setIngredients] = useState([
-    "all the main spices",
-    "pasta",
-    "ground beef",
-    "tomato paste",
-  ]);
+  const [ingredients, setIngredients] = useState([]);
 
   const [recipe, setRecipe] = useState("");
 
